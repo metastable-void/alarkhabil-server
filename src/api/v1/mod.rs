1bd927d99d6e4fc4b2a3eb4bb3f4eecc3754c662
metastable-void <git@yuka.h09.eu>
2023-10-24 20:29:32 +0900

    /api/v1/admin/channel/delete

--- src/api/v1/mod.rs
+++ src/api/v1/mod.rs
@@ -4,8 +4,10 @@ pub mod types;
 mod invite_new;
 mod account_new;
 mod admin_author_delete;
+mod admin_channel_delete;
 
 
 pub use invite_new::api_invite_new;
 pub use account_new::api_account_new;
 pub use admin_author_delete::api_admin_author_delete;
+pub use admin_channel_delete::api_admin_channel_delete;
--- src/bin/alarkhabil.rs
+++ src/bin/alarkhabil.rs
@@ -87,6 +87,7 @@ async fn main() -> anyhow::Result<()> {
         .route("/api/v1/invite/new", get(api::v1::api_invite_new))
         .route("/api/v1/account/new", post(api::v1::api_account_new))
         .route("/api/v1/admin/author/delete", post(api::v1::api_admin_author_delete))
+        .route("/api/v1/admin/channel/delete", post(api::v1::api_admin_channel_delete))
         .with_state(state.clone());
 
     // run server
