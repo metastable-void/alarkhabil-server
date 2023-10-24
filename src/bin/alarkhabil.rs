7fffcd77dc890f918d96cec2fe97a62bb600609f
metastable-void <git@yuka.h09.eu>
2023-10-24 18:40:26 +0900

    refactoring

--- src/bin/alarkhabil.rs
+++ src/bin/alarkhabil.rs
@@ -2,52 +2,22 @@
 use std::env;
 use std::net::SocketAddr;
 use std::str::FromStr;
-use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
-use serde::{Serialize, Deserialize};
-
 use axum::{
-    extract::{State, Query},
+    extract::State,
     routing::{get, post},
     Router,
     Server,
     response::IntoResponse,
-    Json,
 };
 
 use alarkhabil_server::crypto::{PrivateKey, SignedMessage};
-use alarkhabil_server::sys_time;
-use alarkhabil_server::base64;
 use alarkhabil_server::state::{PrimarySecret, AppState};
 use alarkhabil_server::error_reporting::{ErrorReporting, result_into_response};
 
+use alarkhabil_server::api;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Invite {
-    command: String,
-    uuid: String,
-}
-
-impl Invite {
-    fn new() -> Invite {
-        let uuid = uuid::Uuid::new_v4().to_string();
-        Invite {
-            command: "registration_invite".to_string(),
-            uuid,
-        }
-    }
-
-    fn uuid(&self) -> &str {
-        &self.uuid
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct MsgAccountNew {
-    name: String,
-    invite: String,
-}
 
 async fn get_root(
     State(state): State<Arc<AppState>>,
@@ -67,72 +37,6 @@ async fn get_root(
     
         Ok(format!("Hello, world! {} authors", author_count))
     }, ErrorReporting::Html).await
-}
-
-async fn api_invite_new(
-    State(state): State<Arc<AppState>>,
-    Query(params): Query<HashMap<String, String>>,
-) -> impl IntoResponse {
-    result_into_response(async move {
-        let invite_token = state.primary_secret.derive_secret("new_invite_token");
-        let token = hex::decode(params.get("token").ok_or_else(|| anyhow::anyhow!("Missing token parameter"))?)?;
-
-        if invite_token != token {
-            return Err(anyhow::anyhow!("Invalid token"));
-        }
-
-        let signing_key = state.primary_secret.derive_secret("signing_key");
-
-        let msg = Invite::new();
-        let msg = SignedMessage::create(PrivateKey::from_bytes("hmac-sha256", &signing_key)?, serde_json::to_string(&msg)?.as_bytes())?;
-
-        Ok(Json(serde_json::json!({
-            "status": "ok",
-            "invite": base64::encode(serde_json::to_string(&msg)?.as_bytes()),
-        })))
-    }, ErrorReporting::Json).await
-}
-
-async fn api_account_new(
-    State(state): State<Arc<AppState>>,
-    Json(msg): Json<SignedMessage>,
-) -> impl IntoResponse {
-    result_into_response(async move {
-        let public_key = msg.public_key()?.to_owned();
-        let msg = msg.verify()?;
-        let msg = serde_json::from_slice::<MsgAccountNew>(&msg)?;
-        let invite = base64::decode(&msg.invite)?;
-        let signing_key = state.primary_secret.derive_secret("signing_key");
-        let invite: SignedMessage = serde_json::from_slice(&invite)?;
-
-        let key = PrivateKey::from_bytes("hmac-sha256", &signing_key)?;
-        let invite_msg = invite.verify_with_secret(key)?;
-        let invite_msg = serde_json::from_slice::<Invite>(&invite_msg)?;
-
-        let uuid = invite_msg.uuid();
-
-        let mut db_connection = state.db_connection.lock().unwrap();
-        let trx = db_connection.transaction()?;
-
-        if let Ok(_) = trx.query_row("SELECT id FROM author WHERE uuid = ?", [uuid], |row| row.get::<_, u32>(0)) {
-            return Err(anyhow::anyhow!("Author already exists"));
-        }
-
-        let name = &msg.name;
-        let now = sys_time::get_sys_time_in_secs();
-
-        trx.execute("INSERT INTO author (uuid, name, registered_date) VALUES (?, ?, ?)", (uuid, name, now))?;
-        let author_id = trx.query_row("SELECT id FROM author WHERE uuid = ?", [uuid], |row| row.get::<_, u32>(0))?;
-
-        trx.execute("INSERT INTO public_key (author_id, type, public_key) VALUES (?, ?, ?)", (author_id, "ed25519", public_key))?;
-
-        trx.commit()?;
-
-        Ok(Json(serde_json::json!({
-            "status": "ok",
-            "uuid": uuid,
-        })))
-    }, ErrorReporting::Json).await
 }
 
 #[tokio::main]
@@ -180,8 +84,8 @@ async fn main() -> anyhow::Result<()> {
 
     let app = Router::new()
         .route("/", get(get_root))
-        .route("/api/v1/invite/new", get(api_invite_new))
-        .route("/api/v1/account/new", post(api_account_new))
+        .route("/api/v1/invite/new", get(api::v1::api_invite_new))
+        .route("/api/v1/account/new", post(api::v1::api_account_new))
         .with_state(state.clone());
 
     let server = Server::bind(&addr).serve(app.into_make_service());
